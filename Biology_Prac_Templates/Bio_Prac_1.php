fc4ff895a9dfc505d91695ef3761241912fe36d1
PRATHAMESH SUHAS URAVANE <99868564+upratham@users.noreply.github.com>
2025-05-20 22:45:37 +0530

    Update Bio_Prac_1.php

--- Biology_Prac_Templates/Bio_Prac_1.php
+++ Biology_Prac_Templates/Bio_Prac_1.php
@@ -1,3 +1,8 @@
+
+<?php
+include '../header.php';
+$practical_number = "B1";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <script type="module" src="https://unpkg.com/@google/model-viewer@latest"></script> 
